f4a2027b94e56d481f8a665fed695fe201e3d1ba
Rumbidzai Audrey Mateke <rumbidzaiamateke@gmail.com>
2025-08-01 14:05:22 +0000

    added js plugin

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let 
+let poemGenerator = document.getElementById("#poemGen");
+poemGenerator.addEventListener('submit', generatePoem);
+
+function generatePoem(event){
+    event.preventDefault();
+
+    new Typewriter('#poem', {
+  strings: ['Hello', 'World'],
+  autoStart: true,
+});
+
+}
