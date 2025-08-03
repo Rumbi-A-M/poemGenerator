252d28a6960d93651cca79866c5c288446cd4cc5
Rumbidzai Audrey Mateke <rumbidzaiamateke@gmail.com>
2025-08-03 23:26:08 +0000

    bug fixed

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
- function displayPoem(response){
-    
-  new Typewriter("#poem-generator-form", {
+function displayPoem(response) {
+  new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
-
- }
-
+}
 
 function generatePoem(event) {
   event.preventDefault();
-  
-  let instructionInput = document.querySelector('#user-instructions');
-  let apiKey ="b994o32aa80efcbb0t849ced6c60c854";
-  let prompt = `generate a love poem about ${instructionInput} `;
-  let context = "User Instructions are: you are a poet , and love writing poems about love that are short and sweet. Your mission is to generate a six line poem. Make sure to follow the users instruction";
-  let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
- axios.get(apiUrl).then(displayPoem);
-  
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a love poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Love poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
+
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
