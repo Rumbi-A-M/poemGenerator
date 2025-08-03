effaa9b4dcb8df4fff7073b4b4f1dfe40a993bb1
Rumbidzai Audrey Mateke <rumbidzaiamateke@gmail.com>
2025-08-03 23:54:29 +0000

    fixing error in html

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poemOutput");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="poemOuput">⏳ Generating a Love poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="poemOutput">⏳ Generating a Love poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
